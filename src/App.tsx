294f0a243681d2abed61918ba69b03c29b68ea6e
Frank J. Reashore <frank.reashore@gov.bc.ca>
2018-01-08 17:04:41 -0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+import * as React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <h1>React Axios TypeScript App</h1>
+    );
+  }
+}
+
+export default App;
